ec4f86d680fa43e944df5682e1bb22599f745bc8
Todd <todd.ditchendorf@gmail.com>
2013-10-20 19:40:58 -0500

    more tute

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/itod/Envs/env2/src/mysite/templates',
 )
 
 INSTALLED_APPS = (
